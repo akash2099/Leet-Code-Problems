98f0d79be61cfb205ced478bdfc6f956a8470d57
Akash Manna <56742687+akash2099@users.noreply.github.com>
2021-01-21 23:30:40 +0530

    Create 49 Group Anagrams.cpp

---
+++ 49 Group Anagrams.cpp
@@ -0,0 +1,34 @@
+static int fastio=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+private:
+    string strSort(string s) { // better for alphabetical strings
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int c = 0; c < 26; c++) {
+            t += string(counter[c], c + 'a');
+        }
+        return t;
+    }
+        
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            mp[strSort(s)].push_back(s); // sort using counting sort technique
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }
+};
