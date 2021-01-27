9890395b37eb2c6f579af95db5aa99d3cf170444
Akash Manna <56742687+akash2099@users.noreply.github.com>
2021-01-27 16:47:53 +0530

    Create 493 Reverse Pairs.cpp

---
+++ 493 Reverse Pairs.cpp
@@ -0,0 +1,37 @@
+static int fastio=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // # define endl '\n'
+    return 0;
+}();
+class Solution {
+public:
+
+    int sort_and_count(vector<int>::iterator begin, vector<int>::iterator end) {
+        if (end - begin <= 1)
+            return 0;
+        auto mid = begin + (end - begin) / 2;
+        int count = sort_and_count(begin, mid) + sort_and_count(mid, end);
+        for (auto i = begin, j = mid; i != mid; ++i) {
+            while (j != end and *i > 2L * *j)
+                ++j;
+            count += j - mid;
+        }
+        inplace_merge(begin, mid, end);
+        return count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        return sort_and_count(nums.begin(), nums.end());
+    }
+    
+    // int reversePairs(vector<int>& nums) {
+    //     if(nums.size()==0)
+    //         return 0;
+    //     return mergeSort(nums, nums.size());
+    // }
+};
+// better not to use inbuilt functions, added that soln in notes
